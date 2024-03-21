6e6b5e93afb2eeb40405255333008d744ccc97e9
VitorSena0 <97699477+VitorSena0@users.noreply.github.com>
2024-03-21 08:30:26 -0300

    Lógica da leitura do arquivo csv
    formata todos os elementos do csv e transforma em um arquivo json

---
+++ main.js
@@ -0,0 +1,55 @@
+const readLine = require('readline');
+const fs = require('fs');
+
+const line = readLine.createInterface({
+    input: fs.createReadStream('athlete_events.csv'),
+});
+
+const registros = []; // Lista para armazenar os objetos de cada linha
+
+line.on("line", (data) => {
+    const csv = data.split(',');
+
+    // Limpar cada atributo do CSV, removendo as aspas duplas, pontos e vírgulas
+    const cleanedCsv = csv.map(attribute => {
+        const cleanedAttribute = attribute.replace(/[";]/g, '');
+        // Se o atributo for numérico, transforma em número
+        return !isNaN(cleanedAttribute) ? parseFloat(cleanedAttribute) : cleanedAttribute;
+    });
+
+    // Criar um objeto com as propriedades especificadas e adicionar à lista
+    const registro = {
+        ID: cleanedCsv[0],
+        Name: cleanedCsv[1],
+        Sex: cleanedCsv[2],
+        Age: cleanedCsv[3],
+        Height: cleanedCsv[4],
+        Weight: cleanedCsv[5],
+        Team: cleanedCsv[6],
+        NOC: cleanedCsv[7],
+        Games: cleanedCsv[8],
+        Year: cleanedCsv[9],
+        Season: cleanedCsv[10],
+        City: cleanedCsv[11],
+        Sport: cleanedCsv[12],
+        Event: cleanedCsv[13],
+        Medal: cleanedCsv[14]
+    };
+
+    registros.push(registro);
+});
+
+// função que pega o resultado de uma função e escreve em um arquivo JSON
+const mostraResultado = (registros) => {
+    const dados = JSON.stringify(registros, null, 2);
+    fs.writeFile('resultado.json', dados, (err) => {
+        if (err) throw err;
+        console.log('Resultado salvo em resultado.json');
+    });
+};
+
+line.on("close", () => {
+    mostraResultado(registros)
+});
+
+
