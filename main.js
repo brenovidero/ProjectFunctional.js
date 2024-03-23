e1c6ba2840687179ff8eaf28e2cdb2b2de8de1cf
VitorSena0 <97699477+VitorSena0@users.noreply.github.com>
2024-03-22 22:40:35 -0300

    adicionada a função para a manipulação de dados do processamento do csv

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded",  () => {
     // Caminho para o arquivo CSV dentro do diretório do programa
     const filePath = './arquivosCSV/athlete_events.csv';
-  
     // Função para carregar e processar o arquivo CSV
-    function processCSV() {
+    const processCSV = () => {
         fetch(filePath) // Faz uma requisição GET para o arquivo CSV, ou seja, busca o arquivo no servidor e retorna uma Promise com a resposta
             .then(response => response.text()) // O metodo then é chamado quando a Promise é resolvida, e retorna o conteúdo do arquivo CSV como texto
             .then(data => { // O conteúdo do arquivo é passado como argumento para a função
                 const linhas = data.split(/\r?\n/);// Separar linhas usando expressão regular para incluir \r
-  
-                const novaLinha = linhas.map(linha => { // map para processar cada linha do CSV
+
+                const novasLinhas = linhas.map(linha => { // map para processar cada linha do CSV
                     const linhaLimpa = linha.replace(/[";]/g, '');
                     const csv = linhaLimpa.split(',') // separa a linha em um array de strings
                     const cleanedCsv = csv.map(attribute => {// map para limpar cada atributo do CSV, removendo as aspas duplas, pontos e vírgulas
@@ -17,18 +17,17 @@ document.addEventListener("DOMContentLoaded", function() {
                     });
                     const [ID, Name, Sex, Age, Height, Weight, Team, NOC, Games, Year, Season, City, Sport, Event, Medal] = cleanedCsv; // atribui cada valor do array a uma variável
                     const ultimoIndice = cleanedCsv.length;
-                    if(ultimoIndice > 15) {
-                      const eventIndex = 13;
-                      const lastEventIndex = cleanedCsv.findIndex((item, index) => index > eventIndex && item === 'Medal') - 1; // Encontrar o índice do último elemento de Event
-                      const eventValue = cleanedCsv.slice(eventIndex, lastEventIndex + 1).join(','); // Concatenar os elementos de Event
-                      return {ID: ID, Name: Name, Sex: Sex, Age: Age, Height: Height, Weight: Weight, Team: Team, NOC: NOC, Games: Games, Year: Year, Season: Season, City: City, Sport: Sport, Event: eventValue, Medal: cleanedCsv[ultimoIndice - 1] };
-                    }else{
-                        return {ID: ID, Name: Name, Sex: Sex, Age: Age, Height: Height, Weight: Weight, Team: Team, NOC: NOC, Games: Games, Year: Year, Season: Season, City: City, Sport: Sport, Event: Event, Medal: Medal };
-                      }
-  
+                    if (ultimoIndice > 15) {
+                        const eventIndex = 13;
+                        const lastEventIndex = cleanedCsv.indexOf('Medal', eventIndex + 1) - 1; // Encontrar o índice do último elemento de Event
+                        const eventValue = cleanedCsv.slice(eventIndex, lastEventIndex + 1).join(','); // Concatenar os elementos de Event
+                        return { ID: ID, Name: Name, Sex: Sex, Age: Age, Height: Height, Weight: Weight, Team: Team, NOC: NOC, Games: Games, Year: Year, Season: Season, City: City, Sport: Sport, Event: eventValue, Medal: cleanedCsv[ultimoIndice - 1] };
+                    } else {
+                        return { ID: ID, Name: Name, Sex: Sex, Age: Age, Height: Height, Weight: Weight, Team: Team, NOC: NOC, Games: Games, Year: Year, Season: Season, City: City, Sport: Sport, Event: Event, Medal: Medal };
+                    }
+
                 });
-                console.log(novaLinha)
-                //mostraResultado(registros);
+                ManipulaDadosOlimpiadas(novasLinhas);
             })
             .catch(error => {
                 console.error('Ocorreu um erro ao processar o arquivo CSV:', error);
@@ -36,5 +35,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     // Chamar a função para processar o arquivo CSV quando a página for carregada
     processCSV();
-  });
-  
+});
+
+
+    const ManipulaDadosOlimpiadas = (novasLinhas) => {
+        return console.log(novasLinhas);
+    }
