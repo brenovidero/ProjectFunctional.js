98678998197b9065379b81515bd63e49c59523b7
VitorSena0 <97699477+VitorSena0@users.noreply.github.com>
2024-03-22 23:42:05 -0300

    Ajeitei detalhes do processo do csv e adicionei a função da primera questão a ser trabalhada

--- main.js
+++ main.js
@@ -16,28 +16,39 @@ document.addEventListener("DOMContentLoaded",  () => {
                         return !isNaN(attribute) ? parseFloat(attribute) : attribute; // transforma em número se for numérico
                     });
                     const [ID, Name, Sex, Age, Height, Weight, Team, NOC, Games, Year, Season, City, Sport, Event, Medal] = cleanedCsv; // atribui cada valor do array a uma variável
-                    const ultimoIndice = cleanedCsv.length;
-                    if (ultimoIndice > 15) {
-                        const eventIndex = 13;
+                    const ultimoIndice = cleanedCsv.length; // Encontrar o índice do último elemento do array
+                    if (ultimoIndice > 15) { // Se o array tiver mais de 15 elementos, concatenar os elementos de Event
+                        const eventIndex = 13; // Encontrar o índice do elemento Event
                         const lastEventIndex = cleanedCsv.indexOf('Medal', eventIndex + 1) - 1; // Encontrar o índice do último elemento de Event
                         const eventValue = cleanedCsv.slice(eventIndex, lastEventIndex + 1).join(','); // Concatenar os elementos de Event
                         return { ID: ID, Name: Name, Sex: Sex, Age: Age, Height: Height, Weight: Weight, Team: Team, NOC: NOC, Games: Games, Year: Year, Season: Season, City: City, Sport: Sport, Event: eventValue, Medal: cleanedCsv[ultimoIndice - 1] };
-                    } else {
+                    } else { // Se o array tiver 15 elementos, retornar os valores normais
                         return { ID: ID, Name: Name, Sex: Sex, Age: Age, Height: Height, Weight: Weight, Team: Team, NOC: NOC, Games: Games, Year: Year, Season: Season, City: City, Sport: Sport, Event: Event, Medal: Medal };
                     }
 
                 });
-                ManipulaDadosOlimpiadas(novasLinhas);
+                ManipulaDadosOlimpiadas(novasLinhas); // Chama a função para manipular os dados do arquivo CSV
             })
-            .catch(error => {
+            .catch(error => { // O método catch é chamado quando a Promise é rejeitada, e retorna um erro
                 console.error('Ocorreu um erro ao processar o arquivo CSV:', error);
             });
     }
     // Chamar a função para processar o arquivo CSV quando a página for carregada
-    processCSV();
+    processCSV(); // Chama a função para processar o arquivo CSV
 });
 
+    const filtraPrimeiraMulherMedalhaEsporte = (novasLinhas) => (medalha) => (esporte) => {
+        const mulheresMedalhas = novasLinhas.filter(atleta => atleta.Sex === 'F' && atleta.Medal == medalha && atleta.Sport === esporte)
+        .reduce((acc, atleta) => { // Faz um reduce para encontrar a atleta mais antiga
+            if (!acc || acc.Year > atleta.Year) {// Se o ano da atleta for maior que o ano do acumulador, retorna a atleta, ou seja a mais antiga
+                return atleta;
+            } else { 
+                return acc; // Se não, retorna o acumulador, até encontrar a atleta mais antiga
+            }
+        }, null);
+        return console.log(mulheresMedalhas); // Imprimi o resultado da atleta mais antiga
+    }
 
     const ManipulaDadosOlimpiadas = (novasLinhas) => {
-        return console.log(novasLinhas);
+        return filtraPrimeiraMulherMedalhaEsporte(novasLinhas)('Gold')('Diving'); // Filtra as mulheres que ganharam medalhas de ouro no esporte de mergulho, 1º questão
     }
